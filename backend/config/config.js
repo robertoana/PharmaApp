102b05acc6701905e5579a41ec34d82c29d53743
robertoana <robertoana47@gmail.com>
2025-01-07 23:07:59 +0200

    firebase initial setup

---
+++ backend/config/config.js
@@ -0,0 +1,11 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require("../pharmaapp-eff6e-firebase-adminsdk-sp8h7-c78a9a16ff.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+module.exports = db;
