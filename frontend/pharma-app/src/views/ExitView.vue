cb149ba72c487047d57e7c7df3f7a155ea0607ff
robertoana <robertoana47@gmail.com>
2025-01-30 00:02:15 +0200

    added functionality for exit form

--- frontend/pharma-app/src/views/ExitView.vue
+++ frontend/pharma-app/src/views/ExitView.vue
@@ -6,17 +6,31 @@
         <InputGroupAddon>
           <i class="pi pi-box"></i>
         </InputGroupAddon>
-        <Dropdown placeholder="Selectează un medicament" class="w-full" />
+        <Dropdown
+          v-model="selectedMedicine"
+          :options="medicines"
+          optionLabel="name"
+          placeholder="Selectează un medicament"
+          class="w-full"
+        />
       </InputGroup>
 
       <InputGroup>
         <InputGroupAddon>
           <i class="pi pi-receipt"></i>
         </InputGroupAddon>
-        <InputNumber placeholder="Cantitate" class="w-full" />
+        <InputNumber
+          v-model="quantity"
+          placeholder="Cantitate"
+          class="w-full"
+        />
       </InputGroup>
 
-      <Button label="Adaugă" class="p-button-rounded p-button-success mt-3" />
+      <Button
+        label="Adaugă"
+        class="p-button-rounded p-button-success mt-3"
+        @click="verifyStockAndAddMedicineToTransaction"
+      />
     </div>
 
     <div class="form-total">
@@ -31,17 +45,17 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <tr v-for="(item, index) in transactions" :key="index">
+            <td>{{ item.name }}</td>
+            <td>{{ item.price }}</td>
+            <td>{{ item.quantity }}</td>
+            <td>{{ item.subtotal }}</td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
             <td colspan="3" class="total-label">Total</td>
-            <td class="total-value"></td>
+            <td class="total-value">{{ finalPrice }}</td>
           </tr>
         </tfoot>
       </table>
@@ -60,6 +74,8 @@ import InputGroup from 'primevue/inputgroup';
 import InputGroupAddon from 'primevue/inputgroupaddon';
 import InputNumber from 'primevue/inputnumber';
 import Button from 'primevue/button';
+import axios from 'axios';
+// import DateTime from 'luxon';
 
 export default {
   name: 'ExitView',
@@ -69,6 +85,63 @@ export default {
     InputGroupAddon,
     InputNumber,
     Button,
+  },
+  data() {
+    return {
+      medicines: [],
+      transactions: [],
+      selectedMedicine: null,
+      quantity: null,
+      userId: 'urmeaza',
+    };
+  },
+  methods: {
+    async getAllMedicines() {
+      try {
+        const response = await axios.get(
+          'http://localhost:8090/api/medicines/getAllMedicines'
+        );
+        this.medicines = response.data;
+        console.log(this.medicines);
+      } catch (err) {
+        alert(`Error + ${err}`);
+      }
+    },
+    verifyStockAndAddMedicineToTransaction() {
+      if (this.selectedMedicine.quantity <= this.quantity) {
+        alert('Boss e prea mult');
+      } else {
+        const medicineToAdd = {
+          id: this.selectedMedicine.id,
+          name: this.selectedMedicine.name,
+          price: this.selectedMedicine.price,
+          quantity: this.quantity,
+          subtotal: this.quantity * this.selectedMedicine.price,
+        };
+        this.transactions.push(medicineToAdd);
+        console.log(this.transactions);
+        this.emptyForm();
+      }
+    },
+    emptyForm() {
+      this.quantity = null;
+      this.selectedMedicine = null;
+    },
+  },
+  mounted() {
+    this.getAllMedicines();
+    this.emptyForm();
+  },
+  computed: {
+    finalPrice() {
+      let sum = 0;
+      if (this.transactions) {
+        for (const transaction of this.transactions) {
+          sum += transaction.subtotal;
+        }
+      }
+      return sum;
+    },
   },
 };
 </script>
@@ -128,3 +201,16 @@ export default {
   text-align: left;
 }
 </style>
+
+<!-- const medicineToAdd = {
+  id: this.selectedMedicine.id,
+  transactionPrice: this.pricePerTransaction(
+    this.selectedMedicine.quantity,
+    this.selectedMedicine.price
+  ),
+  transactionsDate: DateTime.now()
+    .startof('day')
+    .toFormat("yyyy-MM-dd'T'HH:mm:ssZZ"),
+  userId: 'urmeaza',
+  medicines: [{}],
+}; -->
